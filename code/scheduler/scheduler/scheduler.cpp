c498f0105297adf4af8acd73664f16dad8a36734
Shurney <ashurney@activision.com>
2022-09-19 11:58:29 -0700

    Fixed scheduler vcxproj

---
+++ code/scheduler/scheduler/scheduler.cpp
@@ -0,0 +1,26 @@
+#include "platform.h"
+#include "scheduler.h"
+#include "thread.h"
+#include "task.h"
+
+namespace scheduler
+{
+	struct Scheduler
+	{
+
+	};
+}
+
+namespace scheduler
+{
+
+	namespace thread
+	{
+
+	}
+
+	namespace task
+	{
+
+	}
+}
