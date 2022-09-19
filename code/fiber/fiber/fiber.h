4f3d6edde5ee15479bdd1b644d5aae415a164770
Shurney <ashurney@activision.com>
2022-09-19 05:55:03 -0700

    Added stack over/underrun validation

--- code/fiber/fiber/fiber.h
+++ code/fiber/fiber/fiber.h
@@ -7,6 +7,7 @@ namespace fiber
 	struct Fiber
 	{
 		uintptr_t* sp;
+		uintptr_t* stackCeil;
 		uintptr_t* stackHead;
 	};
 
--- code/fiber/internal/fiber.cpp
+++ code/fiber/internal/fiber.cpp
@@ -15,13 +15,25 @@
 #include <cstring>
 #include "platform.h"
 #include "platform_api.h"
+#include "features.h"
 #include "../fiber/fiber.h"
 
 #define sanity(X) do{ if(!(X)) __debugbreak(); }while(0)
 
 namespace
 {
+	static constexpr uintptr_t END_GUARD = 0xDEADDEADDEADDEADull;
 
+	static void Validate(const fiber::Fiber& f)
+	{
+#if USING(VALIDATE_FIBERS)
+		sanity(*f.stackCeil == END_GUARD && "Stack overrun detected.");
+		sanity(f.sp >= f.stackCeil && "Stack overrun detected.");
+		sanity(f.sp < f.stackHead && "Stack underrun detected.");
+#else //#if USING(VALIDATE_FIBERS)
+		((void)f);
+#endif //#else //#if USING(VALIDATE_FIBERS)
+	}
 }
 
 namespace fiber
@@ -31,9 +43,11 @@ namespace fiber
 		static constexpr unsigned CONTEXT_REG_COUNT = EXTRA_QWORD_COUNT + BASE_REG_COUNT + XMM_REG_COUNT * 2;
 		uintptr_t* const stackCeil = reinterpret_cast<uintptr_t*>(stack);
 		uintptr_t* const stackBase = stackCeil + (stackSize / sizeof(uintptr_t));
-		Fiber out{ stackBase, stackBase };
+		Fiber out{ stackBase, stackCeil, stackBase };
 
+		*stackCeil = END_GUARD;
 		out.sp = InitStackRegisters(stackBase, startAddress, userData, stackSize);
+		Validate(out);
 
 		return out;								   
 	}
@@ -42,13 +56,21 @@ namespace fiber
 	{
 		sanity(toFiber->stackHead[-1] == GetStackStartPlaceholder());
 
+		Validate(*toFiber);
 		StartASM(toFiber->sp);
+		Validate(*toFiber);
 	}
 
 	void Switch(Fiber* curFiber, Fiber* toFiber)
 	{
 		toFiber->stackHead[-1] = curFiber->stackHead[-1]; // copy around the return stack frame pointer
 
+		Validate(*curFiber);
+		Validate(*toFiber);
+
 		SwitchFiberASM(curFiber, toFiber);
+
+		Validate(*toFiber);
+		Validate(*curFiber);
 	}
 }
