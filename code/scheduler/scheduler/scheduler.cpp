af78d16006d3cb0d2fbf5729448c707aa945d0da
Shurney <ashurney@activision.com>
2022-09-22 03:06:56 -0700

    A bit more fleshing out...

--- code/scheduler/scheduler/scheduler.cpp
+++ code/scheduler/scheduler/scheduler.cpp
@@ -3,11 +3,36 @@
 #include "thread.h"
 #include "task.h"
 
+#include "fiber.h"
+
+#include <vector>
+#include <thread>
+#include <unordered_map>
+
+namespace
+{
+	static constexpr unsigned THREAD_WAIT_QUEUE_SIZE = 32;
+
+	struct Task
+	{
+		Task* next;
+		void (*TaskFunc)(void*);
+
+	};
+
+	struct Thread
+	{
+		std::thread thread;
+		spsc::queue 
+	};
+}
+
 namespace scheduler
 {
 	struct Scheduler
 	{
-
+		fiber::FiberAPI fiberAPI;
+		std::unordered_map<unsigned, unsigned> threadIdToIndex;
 	};
 }
 
@@ -20,6 +45,21 @@ namespace scheduler
 	}
 
 	namespace task
+	{
+
+	}
+
+	Scheduler* Create(Options opts)
+	{
+
+	}
+
+	void Destroy(Scheduler* sch)
+	{
+
+	}
+
+	void SetDefault(Scheduler* sch)
 	{
 
 	}
